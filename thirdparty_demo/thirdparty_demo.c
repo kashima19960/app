90ba88251a91aaefc81c07a39c60e6be3018ee7e
kashima19960 <2494389920@qq.com>
2023-12-19 19:17:28 +0800

    6 新建文件夹 thirdparty_demo/ : 实现手动添加第三方软件包并进行编译与使用

---
+++ thirdparty_demo/thirdparty_demo.c
@@ -0,0 +1,12 @@
+#include "ohos_init.h"
+#include "ohos_types.h"
+#include <stdio.h>
+
+#include "myparty.h"
+
+void myparty_demo(void)
+{
+    myparty_test();
+    printf("___________>>>>>>>>>>>>>>>>>>>> [DEMO] Hello world.\n");
+}
+SYS_RUN(myparty_demo);
